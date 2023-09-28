8733ae45cd9e05ddc5e71548adb3967d36c80e53
alicia-lyu <wlyu27@wisc.edu>
2023-09-27 21:18:13 -0500

    Spinner added when image is loading

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -6,9 +6,10 @@ import galleryStyles from "./gallery.module.css"
 import { BiSolidDog } from "react-icons/bi"
 import Image from "next/image"
 import { SelectedContext } from "./context"
+import { Spinner } from "react-bootstrap"
 
 export default function Gallery({ breeds }: { breeds: object }) {
-    const {selected, setSelected} = useContext(SelectedContext)
+    const { selected, setSelected } = useContext(SelectedContext)
 
     function handleClick(event: React.MouseEvent<HTMLAnchorElement>) {
         const target = event.currentTarget as HTMLAnchorElement
@@ -25,7 +26,7 @@ export default function Gallery({ breeds }: { breeds: object }) {
     return <div className={pageStyles.grid}>
         {Object.entries(breeds).map(([breed, subBreeds]) => {
             return (
-                <a key={breed} className={`${pageStyles.card} ${galleryStyles.breed}`} data-breed={breed} onClick={handleClick}>
+                <a key={breed} className={`${pageStyles.card} ${galleryStyles.breedCard}`} data-breed={breed} onClick={handleClick}>
                     <h2><BiSolidDog />{breed}</h2>
                     <DogImage breed={breed} display={selected.includes(breed)} />
                     {subBreeds && <ul>
@@ -43,21 +44,32 @@ export default function Gallery({ breeds }: { breeds: object }) {
 
 function DogImage({ breed, display }: { breed: string, display: boolean }) {
     const [breedImages, setBreedImages] = useState<string[]>([])
+    const [loading, setLoading] = useState(false)
     const breedImage = useMemo(() => pickRandomImage(breedImages), [breedImages])
     useEffect(() => {
+        setLoading(true)
         fetch(`http://localhost:3000/api/${breed}`).then(response => {
             response.json().then(data => {
                 setBreedImages(data.message)
+                setLoading(false)
             })
         })
     }, [breed, display])
-    return display && <Image src={breedImage} alt={`${breed} image`} sizes="100vw"
-        style={{
-            width: '100%',
-            height: 'auto',
-        }}
-        width={500}
-        height={300} />
+    if (display) {
+        if (loading) return <Spinner animation="border" className={galleryStyles.spinner} />;
+        else {
+            return <Image src={breedImage} alt={`${breed} image`} sizes="100vw"
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+                width={500}
+                height={300} />
+            // width and height are only placeholders, the image will be resized to fit the container
+        }
+    } else {
+        return <></>
+    }
 }
 
 function pickRandomImage(images: string[]) {
